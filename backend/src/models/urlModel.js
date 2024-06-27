275b667bed244d3560a95d744e188b2307a519e2
Mansvini Aggarwal <coderspecial@gmail.com>
2024-06-28 05:07:53 +0530

    Consolidating clicks data into usageCount

--- backend/src/models/urlModel.js
+++ backend/src/models/urlModel.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const urlSchema = new mongoose.Schema({
   originalUrl: { type: String, required: true },
   hashedUrl: { type: String, required: true },
-  clicks: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
   usageLimit: { type: Number, default: Infinity },
   usageCount: { type: Number, default: 0 }, 
