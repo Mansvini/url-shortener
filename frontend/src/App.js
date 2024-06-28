53cd0f687874412bfaf67c3fe1cd771e416f919b
Mansvini Aggarwal <coderspecial@gmail.com>
2024-06-28 06:05:16 +0530

    Upgrading frontend project structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,14 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
+import GenerateUrl from './components/GenerateUrl';
 
 function App() {
 
-  const [url, setUrl] = useState('');
-  const [hashedUrl, setHashedUrl] = useState('');
-  const [usageLimit, setUsageLimit] = useState(Infinity);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await axios.post('http://localhost:5001/generate', { 
-      originalUrl: url,
-      usageLimit: usageLimit === '' ? Infinity : usageLimit
-    });
-    setHashedUrl(response.data.hashedUrl);
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <h1>URL Hasher</h1>
-          <form onSubmit={handleSubmit}>
-            <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Enter URL" />
-            <input type="number" placeholder="Usage Limit" value={usageLimit} onChange={(e) => setUsageLimit(e.target.value || Infinity)} min="1" />
-            <button type="submit">Generate</button>
-          </form>
-          {hashedUrl && (
-            <div>
-              <p>Hashed URL: <a href={`http://localhost:5001/a/${hashedUrl}`} target="_blank" rel="noopener noreferrer" className="hashed-link">{`http://localhost:5001/a/${hashedUrl}`}</a></p>
-            </div>
-          )}
-        </div>
-      </header>
+      <div className="App-header">
+        <GenerateUrl/>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/GenerateUrl.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const GenerateUrl = ({ token }) => {
+  const [url, setUrl] = useState('');
+  const [usageLimit, setUsageLimit] = useState(Infinity);
+  const [hashedUrl, setHashedUrl] = useState('');
+
+  const handleGenerate = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5001/a/generate', { 
+        originalUrl: url, 
+        usageLimit: usageLimit === '' ? Infinity : usageLimit 
+      });
+      setHashedUrl(response.data.hashedUrl);
+    } catch (error) {
+      alert('Failed to generate URL');
+    }
+  };
+
+  return (
+    <>
+        <form onSubmit={handleGenerate}>
+            <h2>Generate Hashed URL</h2>
+            <input type="text" placeholder="Enter URL" value={url} onChange={(e) => setUrl(e.target.value)} required />
+            <input type="number" placeholder="Usage Limit" value={usageLimit} onChange={(e) => setUsageLimit(e.target.value)} min="1"/>
+            <button type="submit">Generate</button>
+        </form>
+        {hashedUrl && (
+            <div>
+                <p>Hashed URL: <a href={`http://localhost:5001/a/${hashedUrl}`} target="_blank" rel="noopener noreferrer" className="hashed-link">{`http://localhost:5001/a/${hashedUrl}`}</a></p>
+            </div>
+        )}
+    </>
+  );
+};
+
+export default GenerateUrl;
